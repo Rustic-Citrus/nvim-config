a78126d9f8cbe88d20cc498acf3eb014711a1558
Harry Curtis <books@boothbooks.co.uk>
2025-08-03 15:33:41 +0100

    refactor: modified markdown-renderer.nvim config¬

--- init.lua
+++ init.lua
@@ -999,12 +999,15 @@ require('lazy').setup({
   -- NOTE: Added the render-markdown.nvim plugin.
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+      latex = {
+        enabled = true,
+        renderer = 'dvisvgm',
+      },
+    },
   },
 }, {
   ui = {
